ab062f3fee6fc3cebf71f4c8d774ea6509b0850a
JEAN BERTRAND PAIXAO DA SILVA <jean_bertrand93@hotmail.com>
2022-03-15 14:59:54 -0400

    Update plots_dinamicos.py

--- plots_dinamicos.py
+++ plots_dinamicos.py
@@ -1,16 +1,21 @@
+from turtle import color
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(1,6)
-
+#x = np.arange(1,6)
+x = ['Recomenda','Não \nRecomenda','Compra \nNovamente','Não Compra \nNovamente']
 plt.style.use('ggplot')
 
 plt.ion() #ligando o modo dinamico
-for i in range(10):
-    dados = np.random.randint(20,300,5)
+for i in range(5):
+    dados = np.random.randint(20,300,4) #intervalo de numeros gerados e quantidade de barras (deve coincidir com a quantidade de elementos 'x')
     plt.cla()
     plt.clf()
-    plt.bar(x,dados)
-    plt.pause(3)
+    plt.bar(x,dados,color='red')
+    plt.xlabel('O que foi avaliado')
+    plt.ylabel('Quantidade de Pessoas Avaliadas')
+    plt.title('Pesquisa de satisfação')
+    plt.pause(1)
 
 plt.ioff() #desligando o modo dinamico
+plt.show()
