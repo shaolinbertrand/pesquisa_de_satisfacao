843e8f7e71fc6282edd0565dc675060598848b48
JEAN BERTRAND PAIXAO DA SILVA <jean_bertrand93@hotmail.com>
2022-03-15 12:47:15 -0400

    atualizando o grafico a cada 3 segundos

--- plots_dinamicos.py
+++ plots_dinamicos.py
@@ -2,7 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.arange(1,6)
-dados = np.random.randint(20,30,5)
 
-plt.bar(x,dados)
-plt.show()
+plt.style.use('ggplot')
+
+plt.ion() #ligando o modo dinamico
+for i in range(10):
+    dados = np.random.randint(20,300,5)
+    plt.cla()
+    plt.clf()
+    plt.bar(x,dados)
+    plt.pause(3)
+
+plt.ioff() #desligando o modo dinamico
